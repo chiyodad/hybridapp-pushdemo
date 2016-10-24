b242e409cd066d4bacb0061fe4e15a886061ef2f
david <davidpark1208@gmail.com>
2016-10-25 04:10:07 +0900

    add senderid

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -41,7 +41,7 @@ var app = {
         console.log('calling push init');
         var push = PushNotification.init({
             "android": {
-                "senderID": "XXXXXXXX"
+                "senderID": "20522369309"
             },
             "browser": {},
             "ios": {
